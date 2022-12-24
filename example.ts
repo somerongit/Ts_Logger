1213d4040a66777643f12d5525fbba647fab71ba
Someron Bakuli <68781900+somerongit@users.noreply.github.com>
2022-12-24 21:54:53 +0530

    Create example.ts

---
+++ example.ts
@@ -0,0 +1,18 @@
+import Logger from "./Logger";
+// Initializing Logger with file path 
+const logger: Logger = new Logger(__filename)
+
+/*
+You can set debug mode false by passing false as a second parameter
+   const logger: Logger = new Logger(__filename,false)
+*/
+
+logger.info("Information", "printInfo")
+logger.info(["Abc"], "printInfo")
+
+logger.debug("Debug Information", "printDebug")
+logger.debug(["Abc"], "printDebug")
+
+logger.error("Error Information", "printError")
+logger.error(["Abc"], "printError")
+logger.error("Error: Type Error...", "printError", [{ "d": "4" }, { "e": "5" }], "Input Payload:")
