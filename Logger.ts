a6627412aebd5201c3850426d1ef78d5bf3498f1
Someron Bakuli <68781900+somerongit@users.noreply.github.com>
2023-03-28 15:06:28 +0530

    Update Logger.ts

--- Logger.ts
+++ Logger.ts
@@ -2,6 +2,7 @@ class Logger {
     readonly project_folder_name: string = "Ts_Logger"
     readonly file_path: string
     readonly debug_mode: boolean
+    private formatter: any = new Intl.DateTimeFormat("en-In", { dateStyle: "medium", timeStyle: "medium", hour12: false })
 
     constructor(path: string, debug: boolean = true) {
         let sliceIndex = path.toUpperCase().indexOf(this.project_folder_name.toUpperCase())
@@ -9,15 +10,18 @@ class Logger {
         this.debug_mode = debug
     }
 
-    private log(log_level: string, msg: any, msg_title: any | null = null) {
+    private log(log_level: string, msg_title: any, msg: any | null = null) {
         const time = new Date()
-        const formatter = new Intl.DateTimeFormat("en-In", { dateStyle: "medium", timeStyle: "medium", hour12: false })
-        let log = formatter.format(time) + " " + log_level + ": " + this.file_path
-        console.log(log, msg_title ? msg_title : "", " => ", msg, "\n")
+        let log = log_level + " " + this.formatter.format(time) + ": " + this.file_path
+        if (msg) {
+            console.log(log, msg_title, " => ", msg, "\n")
+        } else {
+            console.log(log, " => ", msg_title, "\n")
+        }
     }
 
-    info(msg: any, msg_title: any | null = null) { this.log("Info", msg, msg_title) }
-    error(msg: any, msg_title: any | null = null) { this.log("Error", msg, msg_title) }
-    debug(msg: any, msg_title: any | null = null) { if (this.debug_mode) this.log("Debug", msg, msg_title) }
+    info(msg_title: any, msg: any | null = null) { this.log("Info", msg_title, msg) }
+    error(msg_title: any, msg: any | null = null) { this.log("Error", msg_title, msg) }
+    debug(msg_title: any, msg: any | null = null) { if (this.debug_mode) this.log("Debug", msg_title, msg) }
 }
 export default Logger
